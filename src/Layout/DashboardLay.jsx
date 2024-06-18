601f4e25fc05fbb189d68f305e9fb3a8791bb64b
sahidDev09 <iamsahidofficial99@gmail.com>
2024-06-18 19:59:46 +0600

    make table for all test and make sidebar fixed

--- src/Layout/DashboardLay.jsx
+++ src/Layout/DashboardLay.jsx
@@ -3,14 +3,14 @@ import Sidebar from "../Components/Dashboard/Sidebar";
 
 const DashboardLay = () => {
   return (
-    <div className=" relative min-h-screen md:flex gap-5">
-      {/*sidebar*/}
-
-      <Sidebar />
-
-      {/* outlet */}
-      <div className=" flex-1">
-        <div className=" p-5">
+    <div className="relative min-h-screen md:flex">
+      {/* Sidebar */}
+      <div className="fixed md:w-72 w-full">
+        <Sidebar />
+      </div>
+      {/* Outlet */}
+      <div className="flex-1 md:ml-72">
+        <div className="p-5">
           <Outlet />
         </div>
       </div>
--- src/Pages/AdminDashboard/Testmanage.jsx
+++ src/Pages/AdminDashboard/Testmanage.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { axiosSecure } from "../../Hooks/useAxiosSecure";
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
 
 const Testmanage = () => {
   const {
@@ -21,6 +23,60 @@ const Testmanage = () => {
       <h1 className=" text-xl font-semibold mt-3">
         Total Tests : {tests.length}
       </h1>
+
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Test</th>
+                <th>Details</th>
+                <th>Actionable Dashboard</th>
+                <th></th>
+              </tr>
+            </thead>
+            {tests.map((test, index) => (
+              <tbody key={index}>
+                <tr>
+                  <th>{index + 1}</th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask rounded-xl w-20 h-20">
+                          <img
+                            src={test.image}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold text-lg">
+                          {test.test_name}
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <span>{test.test_details}</span>
+                  </td>
+                  <th className="flex gap-2">
+                    <button className="btn bg-blue-100 hover:bg-blue-400">
+                      <FaEdit />
+                    </button>
+                    <button className="btn bg-red-100 hover:bg-red-400">
+                      <MdDelete />
+                    </button>
+                    <button className="btn bg-[#C3C1F9] hover:bg-[#8985f6]">
+                      See Reservation
+                    </button>
+                  </th>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
