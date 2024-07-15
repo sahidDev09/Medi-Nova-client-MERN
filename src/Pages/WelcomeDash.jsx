6182ee687cc1a0369442702ead3096659c546859
sahidDev09 <iamsahidofficial99@gmail.com>
2024-07-15 20:43:51 +0600

    lottie in dash

--- src/Pages/WelcomeDash.jsx
+++ src/Pages/WelcomeDash.jsx
@@ -1,5 +1,16 @@
+import Lottie from "lottie-react";
+import lottieDash from '../assets/dashanim.json'
+
 const WelcomeDash = () => {
-  return <div>Welcome Back to Dasboard boss</div>;
+  return <div>
+
+    
+    <div className=" md:w-[70%] w-full mx-auto md:-mt-28 mt-24">
+        <Lottie animationData={lottieDash}></Lottie>
+      </div>
+      <h1 className=" text-5xl font-bold uppercase text-center text-blue-400">DASHBOARD</h1>
+
+  </div>;
 };
 
 export default WelcomeDash;
